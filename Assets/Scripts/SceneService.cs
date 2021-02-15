1057234470a26310db16b340881273d87871c820
Magic Potion Studios <magicpotionstudios@gmail.com>
2021-02-14 23:20:10 -0800

    some title card tweaks, Play Button does a thing

---
+++ Assets/Scripts/SceneService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneService : MonoBehaviour
+{
+    public static SceneService Instance;
+
+    void Awake()
+    {
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void PlayButton(){
+        //TODO: on first boot of game we would start with new game mode play,
+        // in subsequent play's we should should something else
+
+        //Load "first" scene
+        SceneManager.LoadScene("Staging");
+    }
+}
