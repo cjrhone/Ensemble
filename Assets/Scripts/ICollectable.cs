6f18936b1efce7d7415d79d022144873e23052ce
Magic Potion Studios <magicpotionstudios@gmail.com>
2021-02-14 18:58:55 -0800

    add fmod project

--- Assets/Scripts/ICollectable.cs
+++ Assets/Scripts/ICollectable.cs
@@ -8,5 +8,5 @@ public interface ICollectable
     // Indicated whether or not the item has been collected by the player
     bool Collected {get; set;}
 
-    Action OnCollected;
+    event Action OnCollected;
 }
