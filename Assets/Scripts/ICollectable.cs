c25f7d12eece5739872f40af9aae3688e3175021
Magic Potion Studios <magicpotionstudios@gmail.com>
2021-02-14 18:33:30 -0800

    add fmod and some skeleton files

---
+++ Assets/Scripts/ICollectable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectable
+{
+    // Indicated whether or not the item has been collected by the player
+    bool Collected {get; set;}
+
+    Action OnCollected;
+}
