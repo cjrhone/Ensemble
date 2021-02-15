dfbdb1ecb53147f98c1c932298e3f80879ec006a
Magic Potion Studios <magicpotionstudios@gmail.com>
2021-02-14 22:59:45 -0800

    title card fade in and music

---
+++ Assets/Scripts/UI/FadeInButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInButton : MonoBehaviour
+{
+
+    public CanvasGroup CanvasGroup;
+    public float FadeInSeconds;
+
+    void Awake() {
+        CanvasGroup.interactable = false;
+        CanvasGroup.alpha = 0.0f;
+    }
+    
+    public void StartFadingIn(){
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn(){
+        var startTime = Time.time;
+        var endTime = startTime + FadeInSeconds;
+
+        while(Time.time < endTime){
+            var elapsedTime = Time.time - startTime;
+            CanvasGroup.alpha = Mathf.Lerp(0.0f, 1.0f, elapsedTime/FadeInSeconds);
+            yield return new WaitForFixedUpdate();
+        }
+
+        CanvasGroup.interactable = true;
+
+    }
+}
---
+++ Assets/Scripts/UI/FadeInTitle.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FadeInTitle : MonoBehaviour
+{
+
+    public TMP_Text _text;
+    public float FadeInPerCharacterSeconds;
+    public float DelayBetweenCharactersSeconds;
+    public float DelayedStartSeconds;
+    public float ButtonFadeInDelaySeconds;
+    public FadeInButton startButtonToFadeIn;
+    private float _startTime;
+    private TMP_TextInfo _textInfo;
+    private Color _color;
+    private int _visibleCharacters;
+    private Color32[] _newVertexColors;
+
+    void Awake()
+    {
+        _color = _text.color;
+        _color.a = 0.0f;
+        _text.color = _color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textInfo = _text.textInfo;
+        StartCoroutine(FadeInText());
+    }
+
+
+    private IEnumerator FadeInText(){
+        _text.ForceMeshUpdate();
+
+        yield return new WaitForSeconds(DelayedStartSeconds);
+
+        for(var character = 0; character < _textInfo.characterCount; character++){
+            if(!_textInfo.characterInfo[character].isVisible){
+                continue;
+            }
+            var meshIndex = _textInfo.characterInfo[character].materialReferenceIndex;
+            _newVertexColors = _textInfo.meshInfo[meshIndex].colors32;
+            var vertexIndex = _textInfo.characterInfo[character].vertexIndex;
+            yield return StartCoroutine(FadeInLetter(vertexIndex));
+
+        }
+
+        yield return new WaitForSeconds(ButtonFadeInDelaySeconds);
+
+        startButtonToFadeIn.StartFadingIn();
+    }
+
+    private IEnumerator FadeInLetter(int vertexIndex) {
+        var startTime = Time.time;
+        var endTime = startTime + FadeInPerCharacterSeconds;
+
+        while(Time.time < endTime){
+            var elapsedTime = Time.time - startTime;
+            var newAlpha = (byte)Mathf.Lerp(0, 255, elapsedTime/FadeInPerCharacterSeconds);
+            _newVertexColors[vertexIndex].a = newAlpha;
+            _newVertexColors[vertexIndex+1].a = newAlpha;
+            _newVertexColors[vertexIndex+2].a = newAlpha;
+            _newVertexColors[vertexIndex+3].a = newAlpha;
+
+
+            _text.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+            yield return new WaitForFixedUpdate();
+
+        }
+
+        yield return new WaitForSeconds(DelayBetweenCharactersSeconds);
+
+    } 
+}
