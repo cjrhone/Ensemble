0b3dbb6f2459b8a3a7608714fb64b578811bc671
Magic Potion Studios <magicpotionstudios@gmail.com>
2021-02-15 14:32:41 -0800

    Added Dash

---
+++ Assets/Scripts/UI/SkillIconTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillIconTracker : MonoBehaviour
+{
+
+    [SerializeField]
+    private PlayerController _playerController;
+
+    public CanvasGroup DashIcon;
+
+    void Awake()
+    {
+        _playerController.OnDashAvailable += OnDashAvailable;
+        _playerController.OnDashUnavailable += OnDashUnavailable;
+    }
+
+    private void OnDashAvailable() {
+        //TODO: a good place to add some juicy animation for the dash icon activating
+        DashIcon.alpha = 1.0f;
+    }
+
+    private void OnDashUnavailable() {
+        //TODO: another good place to add some animations, maybe a shrink and dimming?
+        DashIcon.alpha = 0.5f;
+    }
+
+}
