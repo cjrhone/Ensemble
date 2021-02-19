65b717de44ef247e94c4b9d5b333eb3cf24794ba
CJ Rhone <cjrh1films@gmail.com>
2021-02-19 09:41:17 -0800

    Add enemy spawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start() 
+    {
+        StartCoroutine(EnemyDrop());
+
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(-8, 8);
+            zPos = Random.Range(-10, 10);
+
+            Instantiate(theEnemy, new Vector3(xPos, 0.5f, zPos), Quaternion.identity );
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
